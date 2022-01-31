c3c80523cc07cbac7614a557798a27c59481c37f
dr.D <dazemo-tsafack@stud.hs-bremen.de>
2022-01-31 11:02:24 +0100

    setting of buttons: done - App status: OK. - next step: projects layout

--- src/app/project-window/project-window.component.ts
+++ src/app/project-window/project-window.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectWindowComponent implements OnInit {
 
+  btn_text : boolean = false;
+  btn_thumb : boolean = false;
+  btn_list : boolean = true;
+  btn_box : boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  textStateSwitch() {
+    this.btn_text = !this.btn_text;
+    this.btn_thumb = false
+  }
+
+  thumbStateSwitch(){
+    this.btn_thumb=!this.btn_thumb;
+    this.btn_text = false;
+  }
+
+  listStateSwitch(){
+    this.btn_list = !this.btn_list;
+    this.btn_box = !this.btn_box;
+  }
+
+  boxStateSwitch(){
+    this.btn_box = !this.btn_box;
+    this.btn_list = !this.btn_list;
   }
 
 }
