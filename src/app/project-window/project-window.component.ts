024b9957e6d3382ebf75706722dd8c882c86ff1a
dr.D <dazemo-tsafack@stud.hs-bremen.de>
2022-02-01 18:24:14 +0100

    Setting buttons optimized - App status: OK - now: content__projects

--- src/app/project-window/project-window.component.ts
+++ src/app/project-window/project-window.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-window.component.scss']
 })
 export class ProjectWindowComponent implements OnInit {
-
+  test = "disabled";
   btn_text : boolean = false;
   btn_thumb : boolean = false;
   btn_list : boolean = true;
@@ -18,13 +18,35 @@ export class ProjectWindowComponent implements OnInit {
   }
 
   textStateSwitch() {
-    this.btn_text = !this.btn_text;
-    this.btn_thumb = false
+    if(!this.btn_text && this.btn_box) {
+      this.btn_text = true;
+      this.btn_thumb = false;
+    }else if (!this.btn_text && !this.btn_box){
+      this.btn_text = true;
+      this.btn_thumb = false;
+    }else if (this.btn_text && this.btn_box){
+      this.btn_text = false;
+      this.btn_thumb = true;
+    }else if(this.btn_text && !this.btn_box){
+      this.btn_text = false;
+    }
   }
 
   thumbStateSwitch(){
-    this.btn_thumb=!this.btn_thumb;
-    this.btn_text = false;
+    if(!this.btn_thumb && !this.btn_box) {
+      this.btn_thumb = true;
+      this.btn_text = false;
+    }else if(this.btn_thumb && !this.btn_box){
+      this.btn_thumb = false;
+    }else if(this.btn_thumb && this.btn_box){
+      this.btn_thumb = false;
+      this.btn_text = true;
+    } else if(!this.btn_thumb && this.btn_box){
+      this.btn_thumb = true;
+      this.btn_text = false;
+    }
+
+
   }
 
   listStateSwitch(){
@@ -33,8 +55,17 @@ export class ProjectWindowComponent implements OnInit {
   }
 
   boxStateSwitch(){
-    this.btn_box = !this.btn_box;
-    this.btn_list = !this.btn_list;
+    if(!this.btn_box && !this.btn_text){
+      this.btn_box = true;
+      this.btn_list = false;
+      this.btn_text = true;
+    }else if (!this.btn_box && this.btn_text){
+      this.btn_box = true;
+      this.btn_list = false;
+    }else if (this.btn_box && this.btn_text){
+      this.btn_box = false;
+      this.btn_list = true;
+    }
   }
 
 }
