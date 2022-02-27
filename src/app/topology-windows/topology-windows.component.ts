f0890f3b33575e3d7212c9e711d940ad91dd2251
dr.D <dazemo-tsafack@stud.hs-bremen.de>
2022-02-27 12:19:12 +0100

    App status: OK :: Topology window started :: zoom done :: next step: CLI and mini windows

--- src/app/topology-windows/topology-windows.component.ts
+++ src/app/topology-windows/topology-windows.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopologyWindowsComponent implements OnInit {
 
+  gridState : boolean = true;
+  zoomValue : number = 100;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSwitchGrid(){
+    this.gridState = !this.gridState;
+  }
+
+  onZoomPlus(){
+    this.zoomValue += 5;
+  }
+
+  onZoomMinus(){
+    this.zoomValue -= 5;
+  }
+
+  getGridStatus() : string{
+    if(this.gridState)
+      return ".big-container"
+    return ".big-container-grid-off";
+}
 }
