eab6b0464ca10abf4d3b91905ec8abc0538871b3
dr.D <dazemo-tsafack@stud.hs-bremen.de>
2022-03-01 07:17:00 +0100

    Devices windows done :: now: CLI Windows

--- src/app/topology-windows/topology-windows.component.ts
+++ src/app/topology-windows/topology-windows.component.ts
@@ -12,6 +12,8 @@ export class TopologyWindowsComponent implements OnInit {
 
   gridState : boolean = true;
   zoomValue : number = 100;
+  miniWindowsStates : boolean [] =[false, false, false] // 0: Devices 1: CLI 2: Settings
+
   constructor() { }
 
   ngOnInit(): void {
@@ -28,4 +30,26 @@ export class TopologyWindowsComponent implements OnInit {
   onZoomMinus(){
     this.zoomValue -= 5;
   }
+
+  getHeightDevices() : string {
+    if(!this.miniWindowsStates[0])
+      return '29px'
+    return '297px'
+  }
+
+  getHeightCLI() : string {
+    if(!this.miniWindowsStates[1])
+      return '29px'
+    return '297px'
+  }
+
+  getHeightSettings() : string {
+    if(!this.miniWindowsStates[2])
+      return '29px'
+    return '297px'
+  }
+
+  switchMiniWindowStateDevice(){ this.miniWindowsStates[0] = !this.miniWindowsStates[0]}
+  switchMiniWindowStateCLI(){this.miniWindowsStates[1] = !this.miniWindowsStates[1]}
+  switchMiniWindowStateSettings(){this.miniWindowsStates[2] = !this.miniWindowsStates[2]}
 }
